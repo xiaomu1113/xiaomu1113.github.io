78dc0cb7fb395182f83364eda8f311e3cc76b278
xiaomu1113 <1979409875@qq.com>
2026-08-20 15:34:14 +0800

    Site updated: 2026-08-20 15:34:11

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/08/16/hello-world/"];function toRandomPost(){
+var posts=["2026/08/20/blog-build-steps/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
